ac60715e65f7c05d482eb425f673ac5f29a604de
Jonas Hermsmeier <jhermsmeier@googlemail.com>
2012-07-10 02:05:21 +0200

    Added tf*idf (term frequency–inverse document frequency)

---
+++ speech.js
@@ -0,0 +1,12 @@
+
+var speech = global.speech = {
+  
+  TfIdf: require( './lib/tf-idf' ),
+  
+  // C++ Bindings
+  recognition: { /* not implemented */ },
+  tts: { /* not implemented */ },
+  
+}
+
+module.exports = speech
