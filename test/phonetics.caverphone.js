64894736052e64f947ac32b3d4363ddfb16d234f
jhermsmeier <jhermsmeier@googlemail.com>
2012-11-24 18:54:20 +0100

    Added test/phonetics.caverphone

---
+++ test/phonetics.caverphone.js
@@ -0,0 +1,19 @@
+
+var assert = require( 'assert' )
+var caverphone = require( '../lib/phonetics/caverphone' )
+
+module.exports = {
+  'Caverphone': {
+    
+    'Stevenson': function() { assert.equal( caverphone( 'Stevenson' ), 'STFNSN1111' ) },
+    'Peter': function() { assert.equal( caverphone( 'Peter' ), 'PTA1111111' ) },
+    
+    'Common CP': function() {
+      [ 'add', 'aid', 'at', 'art', 'eat', 'earth', 'head', 'hit', 'hot', 'hold', 'hard', 'heart', 'it', 'out', 'old' ]
+        .forEach( function( w ) {
+          assert.equal( caverphone(w), 'AT11111111' )
+        })
+    } 
+    
+  }
+}
