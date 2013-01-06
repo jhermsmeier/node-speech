fe5859b709ea2219208723bc7edc64b67d127f2a
jhermsmeier <jhermsmeier@googlemail.com>
2013-01-06 16:34:45 +0100

    Updated test index

--- test/index.js
+++ test/index.js
@@ -1,5 +1,11 @@
 
 module.exports = {
+  'Distance': {
+    'Dice':         require( './distance/dice' ),
+    'Jaro':         require( './distance/jaro' ),
+    'Jaro-Winkler': require( './distance/jaro-winkler' ),
+    'Levenshtein':  require( './distance/levenshtein' ),
+  },
   'Phonetics': {
     'Caverphone': require( './phonetics/caverphone' ),
     'Soundex': require( './phonetics/soundex' ),
